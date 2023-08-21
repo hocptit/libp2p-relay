bc39c7a61ebc83585aa63e3900537dc76f97cc0e
Patrick Nguyen <hoc.nguyen@sotatek.com>
2023-08-21 11:35:24 +0700

    feat: init code

---
+++ relay.js
@@ -0,0 +1,26 @@
+import { mplex } from "@libp2p/mplex"
+import { createLibp2p } from "libp2p"
+import { noise } from "@chainsafe/libp2p-noise"
+import { circuitRelayServer } from 'libp2p/circuit-relay'
+import { webSockets } from '@libp2p/websockets'
+import * as filters from '@libp2p/websockets/filters'
+import { identifyService } from 'libp2p/identify'
+const PORT = process.env.PORT || 51986
+const server = await createLibp2p({
+    addresses: {
+        listen: [`/ip4/127.0.0.1/tcp/${PORT}/ws`]
+    },
+    transports: [
+        webSockets({
+            filter: filters.all
+        }),
+    ],
+    connectionEncryption: [noise()],
+    streamMuxers: [mplex()],
+    services: {
+        identify: identifyService(),
+        relay: circuitRelayServer()
+    }
+})
+
+console.log("p2p addr: ", server.getMultiaddrs().map((ma) => ma.toString()))
