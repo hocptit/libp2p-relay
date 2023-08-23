658dea103e4f25f9550f7b3502cd49fb8cf5f11e
Patrick Nguyen <hoc.nguyen@sotatek.com>
2023-08-23 11:20:15 +0700

    feat: add config peer id

--- relay.js
+++ relay.js
@@ -3,24 +3,28 @@ import { createLibp2p } from "libp2p"
 import { noise } from "@chainsafe/libp2p-noise"
 import { circuitRelayServer } from 'libp2p/circuit-relay'
 import { webSockets } from '@libp2p/websockets'
-import * as filters from '@libp2p/websockets/filters'
+// import * as filters from '@libp2p/websockets/filters'
 import { identifyService } from 'libp2p/identify'
 const PORT = process.env.PORT || 51986
-const server = await createLibp2p({
-    addresses: {
-        listen: [`/ip4/127.0.0.1/tcp/${PORT}/ws`]
-    },
-    transports: [
-        webSockets({
-            filter: filters.all
-        }),
-    ],
-    connectionEncryption: [noise()],
-    streamMuxers: [mplex()],
-    services: {
-        identify: identifyService(),
-        relay: circuitRelayServer()
-    }
-})
+async function main() {
+    const server = await createLibp2p({
+        peerId: "12D3KooWHXjksacSFFVYTy1EnCDm24cvBz1vkJSn1bBuMRbu7TbU",
+        addresses: {
+            listen: [`/ip4/127.0.0.1/tcp/${PORT}/ws`]
+        },
+        transports: [
+            webSockets(),
+        ],
+        connectionEncryption: [noise()],
+        streamMuxers: [mplex()],
+        services: {
+            identify: identifyService(),
+            relay: circuitRelayServer()
+        }
+    })
+    console.log(`Node started with id ${node.peerId.toString()}`)
+    console.log('Listening on:')
+    node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
+}
 
-console.log("p2p addr: ", server.getMultiaddrs().map((ma) => ma.toString()))
+main();
