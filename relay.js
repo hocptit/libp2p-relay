dc9676918e2a05620d56e0dd0d8f45cd24f7ef7d
Patrick Nguyen <hoc.nguyen@sotatek.com>
2023-08-23 11:28:07 +0700

    chore: add  config

--- relay.js
+++ relay.js
@@ -3,12 +3,11 @@ import { createLibp2p } from "libp2p"
 import { noise } from "@chainsafe/libp2p-noise"
 import { circuitRelayServer } from 'libp2p/circuit-relay'
 import { webSockets } from '@libp2p/websockets'
-// import * as filters from '@libp2p/websockets/filters'
+import * as filters from '@libp2p/websockets/filters'
 import { identifyService } from 'libp2p/identify'
 const PORT = process.env.PORT || 51986
 async function main() {
     const server = await createLibp2p({
-        peerId: "12D3KooWHXjksacSFFVYTy1EnCDm24cvBz1vkJSn1bBuMRbu7TbU",
         addresses: {
             listen: [`/ip4/127.0.0.1/tcp/${PORT}/ws`]
         },
@@ -22,9 +21,9 @@ async function main() {
             relay: circuitRelayServer()
         }
     })
-    console.log(`Node started with id ${node.peerId.toString()}`)
+    console.log(`Node started with id ${server.peerId.toString()}`)
     console.log('Listening on:')
-    node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
+    server.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
 }
 
 main();
